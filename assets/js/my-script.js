b77584b2e514bdbabbefc42e92f94af592565004
alfarishi-git <ralfarishi@gmail.com>
2023-02-07 20:49:42 +0700

    add: gallery img including init glightbox and swiper js

--- assets/js/my-script.js
+++ assets/js/my-script.js
@@ -22,3 +22,97 @@ $("#myDiv").venomButton({
   avatar: "./assets/avatar.png",
   message: "Apakah gudang x masih tersedia?",
 });
+
+/**
+ * Initiate glightbox
+ */
+const glightbox = GLightbox({
+  selector: ".glightbox",
+});
+
+/**
+ * Init swiper slider with 1 slide at once in desktop view
+ */
+new Swiper(".slides-1", {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+/**
+ * Init swiper slider with 3 slides at once in desktop view
+ */
+new Swiper(".slides-3", {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+/**
+ * Gallery Slider
+ */
+new Swiper(".gallery-slider", {
+  speed: 400,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
+});
