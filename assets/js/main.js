6c000f8fe73c37d6c03631147bcf44d581ca1c97
alfarishi-git <ralfarishi@gmail.com>
2023-02-10 22:48:22 +0700

    move js code from my-script.js to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 (function () {
-  "use strict";
+  ("use strict");
 
   /**
    * Easy selector helper function
@@ -223,4 +223,99 @@
    * Initiate Pure Counter
    */
   new PureCounter();
+
+  /**
+   * WA Venom Button
+   */
+  $("#myDiv").venomButton({
+    position: "right",
+    // phone: "+6282111616226",
+    // chatMessage: "Halo 👋<br><br>Ada yang bisa saya bantu?",
+    // showPopup: false,
+    // nameClient: "PT. Trinitas Prima Sejahtera",
+    // headerTitle: "Business Account",
+    // avatar: "./assets/avatar.png",
+    // message: "Apakah gudang x masih tersedia?",
+  });
+
+  /**
+   * Info Gudang Slider
+   */
+  const infoGudangSwiper = document.querySelector("swiper-container");
+  Object.assign(infoGudangSwiper, {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+    },
+  });
+  infoGudangSwiper.initialize();
+
+  /**
+   * Gallery - Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: ".glightbox",
+  });
+
+  new Swiper(".gallery-slider", {
+    speed: 400,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+  /**
+   * Contact
+   */
+  document.getElementById("btn-contact").addEventListener("click", function () {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Sorry you can not use this feature yet",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  });
 })();
